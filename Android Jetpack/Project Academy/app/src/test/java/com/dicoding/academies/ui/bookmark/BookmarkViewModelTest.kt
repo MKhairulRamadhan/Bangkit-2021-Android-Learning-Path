1afe3b4e168f69f403911a513c8298cb8f330b32
M Khairul Ramadhan <khairulr59@gmail.com>
2021-05-03 15:40:41 +0700

    update testing viewModel and add repository testing

--- Android Jetpack/Project Academy/app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
+++ Android Jetpack/Project Academy/app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -1,23 +1,34 @@
 package com.dicoding.academies.ui.bookmark
 
+import com.dicoding.academies.data.CourseEntity
+import com.dicoding.academies.data.source.AcademyRepository
+import com.dicoding.academies.utils.DataDummy
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-class BookmarkViewModelTest{
-
+@RunWith(MockitoJUnitRunner::class)
+class BookmarkViewModelTest {
     private lateinit var viewModel: BookmarkViewModel
 
+    @Mock
+    private lateinit var academyRepository: AcademyRepository
+
     @Before
-    fun setUp(){
-        viewModel = BookmarkViewModel()
+    fun setUp() {
+        viewModel = BookmarkViewModel(academyRepository)
     }
 
     @Test
-    fun getBookmarks(){
-        val courseEntity = viewModel.getBookmarks()
-        assertNotNull(courseEntity)
-        assertEquals(5, courseEntity.size)
+    fun getBookmark() {
+        Mockito.`when`<ArrayList<CourseEntity>>(academyRepository.getBookmarkedCourses()).thenReturn(DataDummy.generateDummyCourses() as ArrayList<CourseEntity>)
+        val courseEntities = viewModel.getBookmarks()
+        Mockito.verify<AcademyRepository>(academyRepository).getBookmarkedCourses()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
     }
-
 }
