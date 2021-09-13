f2e6f0e3fddda6f0c4de6ea28c66d37953a62b01
MKhairulRamadhan <khairulr59@gmail.com>
2021-09-13 15:40:09 +0700

    init alarm manager

---
+++ Android Beginner/Alarm Manger/app/src/main/java/com/mkhairulramadhan/alarmmanger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mkhairulramadhan.alarmmanger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
